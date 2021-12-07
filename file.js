6e253d53bd5fb0acfb6961299ddf6ea000f4a396
sadikpathan9876 <65956063+sadikpathanprojects@users.noreply.github.com>
2021-12-08 00:02:58 +0530

    Delete file.js

--- file.js
+++
@@ -1,6 +0,0 @@
-function sample(){
-   var mydate =new Date();
-   document.querySelector(".realtime").textContent=mydate.getHours()+":"+mydate.getMinutes()+":"+mydate.getSeconds();
-}
-
-setInterval(sample,1000);
